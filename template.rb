117753ff1c009a0cc0ff5cddad2c9689602d77b4
Ben Vinson <benjamin.vinson@hover.to>
2024-06-22 01:05:20 -0400

    Testing pattern of template.

--- template.rb
+++ template.rb
@@ -6,6 +6,6 @@ RAILS_REQUIREMENT = ">= 7.1.3"
 REPO_LINK = "https://github.com/vertigo-prime/rails-template.git"
 
 
-run "if uname | grep -q 'Darwin'; then pgrep spring | xargs kill -9; fi"
+# run "if uname | grep -q 'Darwin'; then pgrep spring | xargs kill -9; fi"
 
 run "echo 'test'" if yes?("Test?")
